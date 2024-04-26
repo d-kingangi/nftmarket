dedbd6bead1058242bd81b3c044d2d8699703fb0
d-kingangi <itsronduncan@gmail.com>
2024-04-26 19:37:39 +0300

    shell files

---
+++ backend/packages.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+npm install --save 
+bcrypt 
+concurrently
+dotenv 
+express 
+joi 
+jsonwebtoken 
+mssql 
+odemon 
+supertest 
+uuid 
+cors 
+jest
+
+npm install --save-dev 
+@types/bcrypt 
+@types/concurrently 
+@types/dotenv 
+@types/express 
+@types/joi 
+@types/jsonwebtoken
+@types/mssql 
+@types/nodemon 
+@types/supertest 
+@types/uuid 
+@types/jest 
+# jest ts-jest 
+# typescript 
+@types/socket.io 
+@types/cors
---
+++ backend/structure.sh
@@ -0,0 +1,37 @@
+npm init 
+
+# tsc --init
+
+# npm install express
+
+# echo "node_modules/"  >> .gitignore
+
+
+# touch .env
+
+# echo ".env/"  >> .gitignore
+
+# touch .gitignore
+
+# mkdir http
+
+# mkdir src
+
+cd src
+
+touch server.ts
+
+mkdir Config Controllers Database Interfaces Middlewares Routes Validators
+
+cd Config 
+
+touch sql.config.ts
+
+cd ../
+
+cd Database
+
+mkdir Tables Procedures
+
+cd ../
+cd ../
