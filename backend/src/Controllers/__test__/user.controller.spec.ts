2987722b280d928fa9af539c0991b601bbcfe912
d-kingangi <itsronduncan@gmail.com>
2024-05-04 21:59:07 +0300

    backend work: user controller test

---
+++ backend/src/Controllers/__test__/user.controller.spec.ts
@@ -0,0 +1,134 @@
+
+
+// user controller test file
+
+//test  for controller function create_nftmarket_user()
+
+import mssql from 'mssql'
+import { create_nftmarket_user, get_single_nftmarket_user, get_all_nftmarket_users, update_nftmarket_user, delete_nftmarket_user } from '../user.controller'
+
+
+describe('Account created successfully', ()=>{
+
+    let res: any
+
+    beforeEach(()=>{
+      res={
+          status: jest.fn().mockReturnThis(),
+          json: jest.fn().mockReturnThis()
+      }
+    })
+
+    it('Succesfully creates user', async () => {
+        const req = {
+            body:{
+                user_id: 'user_123',
+                wallet_address: 'user_123',
+                profile_img: 'user_123',
+                user_name: 'user_123',
+                email: 'user_123',
+                location : 'user_123',
+                phone: 'user_123',
+                password: 'user_123'
+            }
+        }
+
+        const mockedInput = jest.fn().mockReturnThis() 
+
+        const mockedExecute = jest.fn().mockResolvedValue({rowsAffected: [1]})
+
+        const mockedRequest ={
+            input: mockedInput,
+            execute: mockedExecute
+        }
+
+        const mockedPool ={
+            request: jest.fn().mockReturnValue(mockedRequest)
+          }
+      
+          jest.spyOn(mssql, 'connect').mockResolvedValue(mockedPool as never)
+      
+          await create_nftmarket_user(req as any, res)
+      
+          expect(res.json).toHaveBeenCalledWith({message: "Account created successfully"})
+
+    })
+})
+
+
+//test for controller function get_all_nftmarket_users()
+
+describe('Gets a single user', ()=>{
+
+    let res: any
+    let req: any
+
+    beforeEach(()=>{
+        req={
+            body:{}
+        }
+        res={
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn().mockReturnThis()
+        }
+    })
+
+    it('Successfuly gets all users', async()=>{
+        const mockedResult = [
+            {
+                user_id: 'user_123',
+                wallet_address: 'user_123',
+                profile_img: 'user_123',
+                user_name: 'user_123',
+                email: 'user_123',
+                location : 'user_123',
+                phone: 'user_123',
+                password: 'user_123'
+            },
+            {
+                user_id: 'user_123',
+                wallet_address: 'user_123',
+                profile_img: 'user_123',
+                user_name: 'user_123',
+                email: 'user_123',
+                location : 'user_123',
+                phone: 'user_123',
+                password: 'user_123'
+            }.
+            {
+                user_id: 'user_123',
+                wallet_address: 'user_123',
+                profile_img: 'user_123',
+                user_name: 'user_123',
+                email: 'user_123',
+                location : 'user_123',
+                phone: 'user_123',
+                password: 'user_123'
+            }
+        ]
+
+        const mockedInput = jest.fn().mockReturnThis() 
+
+        const mockedExecute = jest.fn().mockResolvedValue({ recordset: mockedResult })
+
+        const mockedRequest ={
+            input: mockedInput,
+            execute: mockedExecute
+        }
+
+        const mockedPool ={
+            request: jest.fn().mockReturnValue(mockedRequest)
+        }
+
+        jest.spyOn(mssql, 'connect').mockResolvedValue(mockedPool as never)
+
+        await get_all_nftmarket_users(req as any, res)
+
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({ users: mockedResult });
+    })
+})
+
+
+
+//test for controller function get_single_nftmarket_user()
